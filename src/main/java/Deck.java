d97fc917240c7397891fa5df1e4209e3e678dacb
cjscrimmy <craigscrimgeour@yahoo.com>
2020-06-18 16:16:53 +0100

    final commit after end of lab

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -11,4 +11,34 @@ public class Deck {
     public int deckCardCount(){
         return this.cards.size();
     }
+
+    public void addCard(Card card){
+        cards.add(card);
+    }
+
+    public void buildDeck(){
+        Card newCard;
+
+        for (int i = 0; i < Suit.values().length ; i++) {
+            for (int j = 0; j < CardRank.values().length; j++) {
+                newCard = new Card(Suit.values()[i], CardRank.values()[j]);
+                cards.add(newCard);
+            }
+        }
+    }
+
+    public Card removeCard(){
+       return this.cards.remove(0);
+    }
+
+//    public void shuffleDeck() {
+//        cards.shuffle();
+//    }
+
+//    public void shuffleDeck2(ArrayList cards){
+//        Collections.shuffle(cards);
+//    }
+
+
+
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -6,14 +6,42 @@ import static org.junit.Assert.assertEquals;
 public class DeckTest {
 
     private Deck deck;
+    private Card card1;
 
     @Before
     public void before(){
         deck = new Deck();
+        card1 = new Card();
     }
 
     @Test
     public void deckStartsEmpty(){
         assertEquals(0, deck.deckCardCount());
+    }
+
+    @Test
+    public void canAddCardToDeck(){
+        deck.addCard(card1);
+        assertEquals(1, deck.deckCardCount());
+    }
+
+    @Test
+    public void canBuildDeck(){
+        deck.buildDeck();
+        assertEquals(52, deck.deckCardCount());
+    }
+
+    @Test
+    public void canRemoveCardFromDeck(){
+        deck.buildDeck();
+        deck.removeCard();
+        assertEquals(51, deck.deckCardCount());
+    }
+
+    @Test
+    public void canShuffleCards(){
+        deck.buildDeck();
+        deck.shuffleDeck();
+        assertEquals(, deck.)
     }
 }
