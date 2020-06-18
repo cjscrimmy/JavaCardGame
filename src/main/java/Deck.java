19bbb08a0a6764ec5249f44c86a2951803d9aec3
cjscrimmy <craigscrimgeour@yahoo.com>
2020-06-18 14:39:05 +0100

    deck setup and test the deck startsempty

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,4 +1,14 @@
-//public class Deck {
-//
-//    private ArrayList<Card> cards;
-//}
+import java.util.ArrayList;
+
+public class Deck {
+
+    private ArrayList<Card> cards;
+
+    public Deck(){
+        this.cards = new ArrayList<Card>();
+    }
+
+    public int deckCardCount(){
+        return this.cards.size();
+    }
+}
