3818fc5e3a4b69ada6e892749cd9238dd17926f7
cjscrimmy <craigscrimgeour@yahoo.com>
2020-06-18 14:26:01 +0100

    card class written and tested

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,2 +1,32 @@
 public class Card {
+
+    private Suit suit;
+    private CardRank rank;
+
+    public Card(Suit suit, CardRank rank){
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public Suit getSuit(){
+        return this.suit;
+    }
+
+    public CardRank getCardRank(){
+        return this.rank;
+    }
+
+    public int getCardValue(){
+        return this.rank.getValue();
+    }
 }
+
+
+//Create a Deck class with an ArrayList of cards.
+//(Deck should start of empty)
+//
+//Come up with a method to populate the deck of cards.
+//
+//Find a way to shuffle the cards.
+//
+//Create a method to deal a card from the deck.
--- src/main/java/CardRank.java
+++ src/main/java/CardRank.java
@@ -1,5 +1,25 @@
 public enum CardRank {
-    ACE,
-    TWO,
-    THREE,
+    ACE(1),
+    TWO(2),
+    THREE(2),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(10),
+    QUEEN(10),
+    KING(10);
+
+    private final int value;
+
+    CardRank(int value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return this.value;
+    }
 }
